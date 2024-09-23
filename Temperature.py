10727b2bff8f217a75c590e6d34cc1d1298c1df0
KDeepu3227 <deepugmd0317@gmail.com>
2024-09-23 19:56:13 +0530

    TemperatureinCelsius.py

---
+++ Temperature.py
@@ -0,0 +1,22 @@
+'''
+You are given a temperature in Celsius. You need to convert it to Fahrenheit.
+Input and Output Format
+Input Format:
+A single integer celsius.
+Output Format:
+A single line containing the temperature in Fahrenheit.
+To convert a temperature from Celsius to Fahrenheit, you use the following formula:
+1.Multiply the temperature in Celsius by 9/5.
+2.Add 32 to the result of step 1.
+'''
+# Input
+celsius = int(input())
+
+# Conversion
+fahrenheit = (celsius * 9/5) + 32
+
+# Output
+print(fahrenheit)
+'''
+0
+32.0
